7722ee5f367ed944d9971bd79ca098ea8af58537
Obez99 <giuliogx@live.it>
2021-11-09 15:09:11 +0100

    Imports API and VUE, Gets object in console

--- scripts.js
+++ scripts.js
@@ -2,4 +2,15 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: "#app",
+  data: {
+    urlAPI: "https://flynn.boolean.careers/exercises/api/random/mail"
+  },
+  methods: {
+
+  },
+  mounted() {
+    axios.get(this.urlAPI).then((obj) => {
+      console.log(obj)
+    })
+  }
 })
