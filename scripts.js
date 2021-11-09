2c6347d5831c2d20b385e3cb9d32022b00db14da
Obez99 <giuliogx@live.it>
2021-11-09 15:15:47 +0100

    Pushes random emails into "emails" array

--- scripts.js
+++ scripts.js
@@ -3,14 +3,17 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: "#app",
   data: {
-    urlAPI: "https://flynn.boolean.careers/exercises/api/random/mail"
+    urlAPI: "https://flynn.boolean.careers/exercises/api/random/mail",
+    emails: []
   },
   methods: {
 
   },
   mounted() {
-    axios.get(this.urlAPI).then((obj) => {
-      console.log(obj)
-    })
+    for (let i = 0; i < 10; i++) {
+      axios.get(this.urlAPI).then((obj) => {
+        this.emails.push(obj.data.response)
+      })
+    }
   }
 })
