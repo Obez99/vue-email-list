28979aaaaddf7cc036bdb3a3a477b0dd323b6475
Obez99 <giuliogx@live.it>
2021-11-09 15:56:29 +0100

    Only shows emails list when all pending states are completed

--- bonus/scripts.js
+++ bonus/scripts.js
@@ -4,15 +4,16 @@ const app = new Vue({
   el: "#app",
   data: {
     urlAPI: "https://flynn.boolean.careers/exercises/api/random/mail",
-    emails: []
+    emails: [],
+    pendingEmails: null
   },
-  methods: {
 
-  },
   mounted() {
+    this.pendingEmails = 10
     for (let i = 0; i < 10; i++) {
       axios.get(this.urlAPI).then((obj) => {
         this.emails.push(obj.data.response)
+        this.pendingEmails--
       })
     }
   }
